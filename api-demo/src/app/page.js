d968de1ad4eb5b82e6887fb31341be1e150c7a69
Ankita kathiriya <ankitakathiriya1411@gmail.com>
2024-07-27 11:27:57 +0530

    added upload image link in main page.

--- api-demo/src/app/page.js
+++ api-demo/src/app/page.js
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
-
 export default function Home() {
   return (
     <div className={styles.main}>
      <Link href="/addproduct" className="text-3xl underline">Add Products</Link>
      <Link href="/products" className="text-3xl underline">Products</Link>
-
+     <Link href="/uploadimage" className="text-3xl underline">Upload Image</Link>
     </div>
   );
 }
